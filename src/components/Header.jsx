9762feec22f85296a4279a1905f91669d7f7d661
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-02-18 23:22:29 +0600

    (feat) Completed Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import Search from '/search.svg'
 import Cart from '/cart.svg'
 import User from '/user.svg'
 import { Link, NavLink } from 'react-router'
+import Burger from '/Burger.svg'
 
 const Header = () => {
   return (
@@ -14,24 +15,28 @@ const Header = () => {
           <Link to='/'> <img src={Logo} alt="Logo" className='w-full' /></Link>
         </div>
         {/* Search  */}
-        <div className="search flex items-center px-4 gap-x-2 bg-[#F5F5F5] rounded-lg h-14 w-[30vw]">
+        <div className="search hidden lg:flex items-center px-4 gap-x-2 bg-[#F5F5F5] rounded-lg h-14 w-[30vw]">
           <img src={Search} alt="" className='w-4 h-4' />
           <input className='w-full h-full outline-none text-[#656565] text-sm' type="text" placeholder="Search here" />
         </div>
         {/* Menu  */}
-        <div className="menu">
+        <div className="menu hidden lg:block">
           <ul className='flex gap-x-12'>
-            <li> <NavLink style={({ isActive }) => ({fontWeight: isActive ? "bold" : "normal"})} to='/'>Home</NavLink> </li>
-            <li> <NavLink style={({ isActive }) => ({fontWeight: isActive ? "bold" : "normal"})} to='about'>About</NavLink> </li>
-            <li> <NavLink style={({ isActive }) => ({fontWeight: isActive ? "bold" : "normal"})} to='contact'>Contact Us</NavLink> </li>
-            <li> <NavLink style={({ isActive }) => ({fontWeight: isActive ? "bold" : "normal"})} to='blog'>Blog</NavLink> </li>
+            <li> <NavLink style={({ isActive }) => ({ fontWeight: isActive ? "bold" : "normal" })} to='/'>Home</NavLink> </li>
+            <li> <NavLink style={({ isActive }) => ({ fontWeight: isActive ? "bold" : "normal" })} to='about'>About</NavLink> </li>
+            <li> <NavLink style={({ isActive }) => ({ fontWeight: isActive ? "bold" : "normal" })} to='contact'>Contact Us</NavLink> </li>
+            <li> <NavLink style={({ isActive }) => ({ fontWeight: isActive ? "bold" : "normal" })} to='blog'>Blog</NavLink> </li>
           </ul>
         </div>
         {/* Icons  */}
-        <div className="icons flex gap-x-6">
+        <div className="icons hidden lg:flex gap-x-6">
           <img className='w-5 h-5 cursor-pointer' src={Wishlist} alt="heart icon" />
           <img className='w-5 h-5 cursor-pointer' src={Cart} alt="Cart icon" />
           <img className='w-5 h-5 cursor-pointer' src={User} alt="User icon" />
+        </div>
+        {/* Mobile Menu  */}
+        <div className="w-10 lg:hidden">
+          <img src={Burger} alt="" className='w-full cursor-pointer'/>
         </div>
       </section>
     </>
